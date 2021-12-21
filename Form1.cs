4344773ccc06c0d7a429c68f292f11c869895465
ncw3@ecogif.co <rajkirant@live.com>
2021-12-21 21:49:14 +0000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading;
+
+namespace Power_WordPad
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+    }
+ 
+}
