8bb20be2f363e40362f33568fbad7e69f39dbd0f
ncw3@ecogif.co <rajkirant@live.com>
2021-12-21 22:04:53 +0000

    parameterized thread

--- Form1.cs
+++ Form1.cs
@@ -10,13 +10,22 @@ namespace Power_WordPad
         {
             InitializeComponent();
         }
+        
         private void button1_Click(object sender, EventArgs e)
         {
+            Thread t1 = new Thread(Threads.scanText);
+            t1.Start("test");
         }
         private void Form1_Load(object sender, EventArgs e)
         {
         }
 
     }
- 
+    public class Threads
+    {
+        public static void scanText(object pattern)
+        {
+            MessageBox.Show(pattern.ToString());
+        }
+    }
 }
