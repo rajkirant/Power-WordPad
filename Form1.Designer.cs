36c986249b7388853362b1ab369842cb9c295341
ncw3@ecogif.co <rajkirant@live.com>
2021-12-22 20:12:47 +0000

    small level working prototype

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,34 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // richTextBox1
             // 
-            this.button1.Location = new System.Drawing.Point(556, 321);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.richTextBox1.Location = new System.Drawing.Point(1, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(797, 415);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "president of usa i";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 388);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(797, 27);
+            this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private Button button1;
+        private RichTextBox richTextBox1;
+        private TextBox textBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,31 +1,105 @@
-using System;
-using System.Threading;
-
+using System.Net;
+using System.Text.RegularExpressions;
 namespace Power_WordPad
+
+
 {
 
     public partial class Form1 : Form
     {
+      
+        string validateInputSentance = @"^([a-zA-Z ]{2,20}) of ([a-zA-Z ]{2,20}) is ([a-zA-Z ]{2,20}).$";
+        string IncumbentExtract = "<b>Incumbent<br />[a-zA-Z<=\"/_>. ]+\">([a-zA-Z .]+)</a>";
+        AutoCompleteStringCollection autoText = new AutoCompleteStringCollection();
+        
+        private string fetchinfo(string url, string[] patterns)
+        {
+            WebRequest request = WebRequest.Create(url);
+            Console.WriteLine(url);
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            System.IO.Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            foreach (string pattern in patterns)
+            {
+                Match m = Regex.Match(responseFromServer.Trim(), pattern);
+                if (m.Success)
+                {
+                    return m.Groups[1].ToString();
+                }
+            }
+            return null;
+        }
         public Form1()
         {
             InitializeComponent();
         }
-        
-        private void button1_Click(object sender, EventArgs e)
+
+   
+        public void scanText(object pattern)
         {
-            Thread t1 = new Thread(Threads.scanText);
-            t1.Start("test");
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
+         
+                this.Invoke((MethodInvoker)delegate ()
+                {
+                Match m = Regex.Match(richTextBox1.Text.Trim(), validateInputSentance);
+                    if (m.Success)
+                    {
+                        System.Diagnostics.Debug.WriteLine("success");
+                        string[] patterns = { IncumbentExtract };
+                        string toBeValidatedData = m.Groups[3].ToString();
+                        string correctData = fetchinfo("https://en.wikipedia.org/w/index.php?search=" + m.Groups[1] + " of " + m.Groups[2], patterns);
+                        if (toBeValidatedData.Length < 3 || !correctData.ToLower().Contains(toBeValidatedData.ToLower()))
+                        {
+                            int startIndexVerifyData = richTextBox1.Text.IndexOf(toBeValidatedData);
+                            richTextBox1.Select(startIndexVerifyData, toBeValidatedData.Length);
+                            richTextBox1.SelectionColor = Color.Red;
+                            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                            richTextBox1.SelectionLength = 0;
+                        }
+                        textBox1.Text = m.Groups[1].ToString() + " of " + m.Groups[2].ToString() + " is " + correctData;
+
+                    }
+                });
+               
+            
         }
 
-    }
-    public class Threads
-    {
-        public static void scanText(object pattern)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show(pattern.ToString());
+           // richTextBox1.Select(0, richTextBox1.Text.Length);
+
+        }
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+            if (e.KeyData.ToString() == "OemPeriod")
+            {
+                new Thread(scanText).Start(validateInputSentance);
+
+            }
+            else if (e.KeyData.ToString() == "Back")
+            {
+                richTextBox1.Select(0, richTextBox1.Text.Length);
+                richTextBox1.SelectionColor = Color.Black;
+                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                richTextBox1.SelectionLength = 0;
+                
+                //e.SuppressKeyPress = true;
+
+            }
+
+
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+           
         }
     }
+
 }
